9a50d6dd0f51869e5566e872b216126a7721a008
Diwura <boluroajayia@gmail.com>
2026-04-07 13:14:27 +0100

    Add Location column to Customers

--- Tables/Customers.sql
+++ Tables/Customers.sql
@@ -3,5 +3,6 @@ CREATE TABLE [dbo].[Customers]
     [CustomerID] INT NOT NULL PRIMARY KEY,
     [FirstName] NVARCHAR(50) NOT NULL,
     [LastName] NVARCHAR(50) NOT NULL,
-    [Email] NVARCHAR(100) NULL
+    [Email] NVARCHAR(100) NULL,
+    [Location] NVARCHAR(100) NULL
 );
